13b8f1c6c9e163500ac75d44cdab6933795d8235
zluchetti <90871914+zluchetti@users.noreply.github.com>
2021-11-17 15:46:43 -0700

    Create problem2.ino

---
+++ problem2.ino
@@ -0,0 +1,12 @@
+
+ void setup() {
+   pinMode(11, OUTPUT);
+   pinMode(10, OUTPUT);
+   pinMode(9, OUTPUT);
+   analogWrite(11, 2);//green
+   analogWrite(10, 16);//blue
+   analogWrite(9, 128);//red
+ }
+ void loop() {
+   
+ }
